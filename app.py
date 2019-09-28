6294e6f8fa511dda1946846048d33a96c3aaed54
Sorrel Bri <sorrel@iPhone7-Plus.localdomain>
2019-09-28 14:03:27 -0700

    config api

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
+# base directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# dev database
+DATABASE = 'postgresql://localhost/browser-go'
+
+# config database
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# init database
+db = SQLAlchemy(app)
+
+# init marshmallow
+ma = Marshmallow(app)
+
+# dev server
 DEBUG = True
 PORT = 8000
+
+# Routes
 
 @app.route('/')
 def hello_world():
