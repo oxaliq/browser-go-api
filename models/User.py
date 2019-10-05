75f244ae9f2baad88d680d1950e7bc1c29437576
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-10-04 23:25:01 -0700

    add auth routes

--- models/User.py
+++ models/User.py
@@ -1,6 +1,7 @@
 from app import db, ma, bcrypt
 import datetime
 import enum
+import jwt
 
 class Ranks(enum.Enum): # with minimal Elo rating
     D7 = "Seven Dan" # Elo 2700+
@@ -61,3 +62,37 @@ class User(db.Model):
         ).decode()
         self.registered_on = datetime.datetime.now()
         self.admin = admin
+
+    def encode_auth_token(self, user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            app.config.get('SECRET_KEY'),
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
