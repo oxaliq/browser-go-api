90fa05c10eb5e83638cd5b05c2f132404392a52c
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-10-12 11:28:21 -0700

    add / message

--- server.py
+++ server.py
@@ -10,8 +10,19 @@ from websockets.socket import socketio
 import configuration.models_mount
 from flask_migrate import Migrate
 
+from flask import Blueprint, jsonify
+
+server = Blueprint('server', __name__, url_prefix='/')
+
+@server.route('/', methods=['GET'])
+def api_home():
+    response = {"message": "hello world"}
+    return jsonify(response)
+
 if __name__ == '__main__':
     app = create_app()
     register_api_endpoints(app)
+    app.register_blueprint(server)
     migrate = Migrate(app, db)
     socketio.run(app, debug=True)
+
