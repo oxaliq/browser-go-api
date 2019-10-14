fb4324cb42e7700f841340d1a129803173663c13
Sorrel Bri <sorrel@SBJ-comp.local>
2019-10-14 16:40:29 -0700

    try add psycopg2 connection

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ import configuration.models_mount
 from flask_migrate import Migrate
 
 from flask import Blueprint, jsonify
-
+import psycopg2
 server = Blueprint('server', __name__, url_prefix='/')
 
 @server.route('/', methods=['GET'])
@@ -24,5 +24,9 @@ if __name__ == '__main__':
     register_api_endpoints(app)
     app.register_blueprint(server)
     migrate = Migrate(app, db)
+    # added 10/14
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    
     socketio.run(app, debug=False)
     
