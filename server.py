0c277f26d75b711bb7489a91abdd064f5271ce0c
Sorrel Bri <sorrel.bri.june@gmail.com>
2019-10-25 10:17:50 -0700

    turn off debug for deploy

--- server.py
+++ server.py
@@ -29,4 +29,4 @@ DATABASE_URL = os.environ['DATABASE_URL']
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
